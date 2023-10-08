15db95011cae78390fe9b4503b99b510ff571e7d
travis fernandes <travisfernandes2327@gmail.com>
2023-10-08 18:14:21 +0530

    react-app_with_MUI

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+
+function Header() {
+  return (
+    <header>
+      <h1><AutoStoriesIcon sx={{fontSize:25}}/> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
